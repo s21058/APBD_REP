e666a2399591bff14e0f7cfcbedcc94403a73c9c
Sviatoslav Bozhko <101318823+s21058@users.noreply.github.com>
2022-04-20 22:38:30 +0200

    Add files via upload

---
+++ WebApplication2/WebApplication2/Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using WebApplication2.Services;
+
+namespace WebApplication2.Controllers;
+
+public class BooksController
+{
+    private readonly IDbServise _dbServise;
+
+    public BooksController(IDbServise dbServise)
+    {
+        _dbServise = dbServise;
+    }
+    [HttpGet]
+    public IList<Book> getBooks()
+    {
+        return _dbServise.getBookList();
+    }
+}
---
+++ WebApplication2/WebApplication2/Services/DbService.cs
@@ -0,0 +1,12 @@
+﻿using WebApplication2.Models;
+
+namespace WebApplication2.Services;
+
+public class DbService:IDbServise
+{
+    public <IList<Book>> getBookList()
+    {
+        
+    }
+
+}
